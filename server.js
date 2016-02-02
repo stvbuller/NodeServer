ecfeeb0a5ef08df32d7a95dd80d907929ae00a03
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-01 20:38:34 -0500

    created html pages

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var http = require('http')
 
 
 var PORTONE = 7000;
-var PORTTEO = 7500;
+var PORTTWO = 7500;
+
+
+var goodResponse = ["You're great!", "You're Wonderful!", "Your Awesome"]
+var badResponse = ["You're not great!", "You're not Wonderful!", "Your not Awesome"]
+
 
 
 var handleRequestOne = function (req, res) {
-  res.end("You're great! You hit: " + req.url);
+  var num = getRandomInt(0, goodResponse.length);
+  res.end(goodRespose[num] + req.url);
 }
 
 
@@ -16,4 +22,12 @@ var handleRequestTwo = function (req, res) {
   res.end("You're not so great! You hit: " + req.url);
 }
 
+serverOne.listen(PORTONE, function() {
+  console.log("Server is listening at http://localhost:%s", PORTONE);
+});
 var serverTwo = http.createServer(handleRequestTwo);
+
+serverTwo.listen(PORTTWO, function() {
+  console.log("Server is listening at http://localhost:%s", PORTTWO);
+});
+
---
+++ server1.js
@@ -0,0 +1,65 @@
+var http = require('http'), url = require('url');
+
+var PORT = 8080;
+
+var handleRequest = function (req, res) {
+  var url_parts = url.parse(req.url);
+  switch (url_parts.pathname) {
+    case '/':
+      display_root(req, res);
+      break;
+    case '/Food':
+      display_food(req, res);
+      break;
+    case '/Movies':
+      display_movies(req, res);
+      break;
+    case '/CSS Frameworks':
+      display_css(req, res);
+      break;
+    default:
+      display_404(req, res);
+      break;
+  }
+}
+
+var display_root = function(req, res) {
+  var myHTML = "<html><body>";
+  myHTML += "<h1>Home page!</h1>";
+  myHTML += "<a href='/test'>Test Page</a>";
+  myHTML += "</body></html>";
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(myHTML);
+}
+
+var display_food = function(req, res) {
+  var myHTML =
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(myHTML);
+}
+
+var display_movies = function(req, res) {
+  var myHTML = 
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(myHTML);
+}
+ 
+var display_css = function(req, res) {
+  var myHTML = 
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(myHTML);
+}
+
+var display_404 = function(req, res) {
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  res.write("<h1>404 Not found</h1>");
+  res.end("This is not it!");
+}
+
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+  console.log("Server is listening at http://localhost:%s", PORT);
+});
+
